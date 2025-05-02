7e1fee652486005adf13b0af8eb88d3c7ab5aa5a
Farmanmann <farmannali222@gmail.com>
2025-05-01 23:53:35 -0500

    Upload Script with batch writefunction

--- utils/upload_datasets_to_dynamodb.py
+++ utils/upload_datasets_to_dynamodb.py
@@ -4,33 +4,34 @@ from tqdm import tqdm
 
 dynamodb = boto3.resource("dynamodb", region_name="us-east-2")
 
-def upload_to_dynamodb(table_name, df, key_fn):
+def batch_upload(table_name, df, key_fn):
     table = dynamodb.Table(table_name)
-    for _, row in tqdm(df.iterrows(), total=len(df), desc=f"Uploading to {table_name}"):
-        item = {k: str(v) if pd.notna(v) else None for k, v in row.items()}
-        item.update(key_fn(row))
-        table.put_item(Item=item)
+    with table.batch_writer(overwrite_by_pkeys=list(key_fn(df.iloc[0]).keys())) as batch:
+        for _, row in tqdm(df.iterrows(), total=len(df), desc=f"Uploading to {table_name}"):
+            item = {k: str(v) if pd.notna(v) else None for k, v in row.items()}
+            item.update(key_fn(row))
+            batch.put_item(Item=item)
 
 def main():
-    # City
+    # Upload GlobalLandTemperaturesByCity.csv
     df_city = pd.read_csv("datasets/GlobalLandTemperaturesByCity.csv").dropna(subset=["City", "Country", "dt"])
-    upload_to_dynamodb(
+    batch_upload(
         "GlobalLandTemperaturesByCity",
         df_city,
         lambda r: {"CityCountry": f"{r['City']}_{r['Country']}", "dt": r["dt"]}
     )
 
-    # State
+    # Upload GlobalLandTemperaturesByState.csv
     df_state = pd.read_csv("datasets/GlobalLandTemperaturesByState.csv").dropna(subset=["State", "Country", "dt"])
-    upload_to_dynamodb(
+    batch_upload(
         "GlobalLandTemperaturesByState",
         df_state,
         lambda r: {"StateCountry": f"{r['State']}_{r['Country']}", "dt": r["dt"]}
     )
 
-    # Country
+    # Upload GlobalLandTemperaturesByCountry.csv
     df_country = pd.read_csv("datasets/GlobalLandTemperaturesByCountry.csv").dropna(subset=["Country", "dt"])
-    upload_to_dynamodb(
+    batch_upload(
         "GlobalLandTemperaturesByCountry",
         df_country,
         lambda r: {"Country": r["Country"], "dt": r["dt"]}
@@ -38,4 +39,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
